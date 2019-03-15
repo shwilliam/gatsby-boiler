6d34cc4f58282a8d432583c3d400a652f0aea2b7
William L <w-lindvall@outlook.com>
2019-03-15 01:04:03 -0400

    remove reset.css

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
-import '../styles/reset.css'
 import '../styles/tachyons.min.css'
 import '../styles/utilities.css'
 
